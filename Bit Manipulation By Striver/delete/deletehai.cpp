5c278c6786a2f63495fbc1d82d69e73a5f17cf27
Mudassir Qureshi <mudassirmohammad7987@gmail.com>
2025-06-23 11:55:35 -0700

    Random

---
+++ Bit Manipulation By Striver/delete/deletehai.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5; // 0101 in binary
+    int k = 2; // Clear the 2nd bit (1-based index)
+    
+    int result;
+    result = n & ~(1 << (k - 1));
+    cout << "Result after clearing the " << k << "th bit: " << result << endl; // Should print 1 (0001 in binary)
+
+    return 0;
+}
