e6f4f906b3232aa4fe645aac29aa66e80cfa1f96
Mudassir Qureshi <mudassirmohammad7987@gmail.com>
2025-07-22 09:02:36 -0700

    Random

---
+++ Problem Solving/random2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is a random code snippet for testing purposes." << endl;
+    cout << "It is designed to demonstrate basic C++ syntax and output." << endl;
+    cout << "Feel free to modify and use it as needed." << endl;
+    return 0;
+}
