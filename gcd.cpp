3c95fab3e8c1b7da02edb4870ccac1f0248e4a8f
MudassirQureshi7987 <mudassirmohammad7987@gmail.com>
2025-11-12 12:09:23 +0530

    GCD

--- gcd.cpp
+++ gcd.cpp
@@ -1,27 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <numeric>  // for std::gcd
 using namespace std;
 
-int gcd(int a,int b)
+int gcdManual(int a,int b)
 {
     if(a==0)
-    {
         return b;
-    }
     if(b==0)
-    {
         return a;
-    }
 
     while(a!=b)
     {
         if(a>b)
-        {
             a = a - b;
-        }
-        else if(b>a)
-        {
+        else
             b = b - a;
-        }
     }
     return a;
 }
@@ -32,6 +25,11 @@ int main()
     cout<<"Enter the numbers:";
     cin>>a>>b;
 
-    int ans = gcd(a,b);
-    cout<<"GCD of "<<a<<" and "<<b<<" is "<<ans;
+    int ans = gcdManual(a,b);
+    cout<<"GCD (manual) of "<<a<<" and "<<b<<" is "<<ans;
+
+    int ans2 = std::gcd(a,b);   // ✅ Corrected
+    cout<<"\nGCD (using std::gcd) of "<<a<<" and "<<b<<" is "<<ans2;
+
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,41 @@
 #include<iostream>
-#include<math.h>
+#include <numeric>  // for std::gcd
 using namespace std;
+
+int gcdManual(int a,int b)
+{
+    if(a==0)
+    {
+        return b;
+    }
+    if(b==0)
+    {
+        return a;
+    }
+
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a = a - b;
+        }
+        else if(b>a)
+        {
+            b = b - a;
+        }
+    }
+    return a;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the numbers:";
+    cin>>a>>b;
+
+    int ans = gcdManual(a,b);
+    cout<<"GCD of "<<a<<" and "<<b<<" is "<<ans;
+
+    int ans2 = gcd(a,b);
+    cout<<"\nGCD (using manual gcd) of "<<a<<" and "<<b<<" is "<<ans2;
+}
